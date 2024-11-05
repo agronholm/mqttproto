b837ba58bcf374dbac8b90051e42880699fa3ae2
Alex Grönholm <alex.gronholm@nextday.fi>
2024-11-05 18:11:02 +0200

    Added the sphinx_rtd_theme extension
    Their docs say it needs to be loaded as an extension for the JS stuff to work.

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
     "sphinx_tabs.tabs",
     "enum_tools.autoenum",
 ]
