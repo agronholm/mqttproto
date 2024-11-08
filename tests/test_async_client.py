ebc4dcb8a4a269380e0e0458b9b1fd74fe1751b7
Matthias Urlichs <matthias@urlichs.de>
2024-11-08 18:30:18 +0100

    Test all combinations of publish+subscribe QoSes

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -8,12 +8,13 @@ from mqttproto.async_client import AsyncMQTTClient
 pytestmark = [pytest.mark.anyio, pytest.mark.network]
 
 
-@pytest.mark.parametrize("qos", [QoS.AT_MOST_ONCE, QoS.AT_LEAST_ONCE, QoS.EXACTLY_ONCE])
-async def test_publish_subscribe(qos: QoS) -> None:
+@pytest.mark.parametrize("qos_sub", [QoS.AT_MOST_ONCE, QoS.AT_LEAST_ONCE, QoS.EXACTLY_ONCE])
+@pytest.mark.parametrize("qos_pub", [QoS.AT_MOST_ONCE, QoS.AT_LEAST_ONCE, QoS.EXACTLY_ONCE])
+async def test_publish_subscribe(qos_sub: QoS, qos_pub:QoS) -> None:
     async with AsyncMQTTClient() as client:
-        async with client.subscribe("test/+") as messages:
-            await client.publish("test/text", "test åäö", qos=qos)
-            await client.publish("test/binary", b"\x00\xff\x00\x1f", qos=qos)
+        async with client.subscribe("test/+", maximum_qos=qos_sub) as messages:
+            await client.publish("test/text", "test åäö", qos=qos_pub)
+            await client.publish("test/binary", b"\x00\xff\x00\x1f", qos=qos_pub)
             packets: list[MQTTPublishPacket] = []
             async for packet in messages:
                 packets.append(packet)
